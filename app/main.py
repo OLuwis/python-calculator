158f2027e5730fcd69eb8577839764bac6a53bb9
Luis Miguel <oluismrs@gmail.com>
2024-06-20 04:20:43 -0300

    remove global res import

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
-import resources.resources
-from views.window import Window
 from PySide6.QtWidgets import QApplication
 from PySide6.QtGui import QFontDatabase
+from res.resources import qInitResources
+from views.window import Window
 
 if __name__ == "__main__":
   app = QApplication([])
 
+  qInitResources()
   QFontDatabase.addApplicationFont(":/fonts/opensans")
   
   window = Window()
