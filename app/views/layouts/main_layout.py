9c0cfeed710925fe90f35e04dac4504453e14d7d
Luis Miguel <oluismrs@gmail.com>
2024-06-20 04:23:42 -0300

    refactor main window and layout

---
+++ app/views/layouts/main_layout.py
@@ -0,0 +1,23 @@
+from PySide6.QtWidgets import QVBoxLayout, QWidget
+from utils.display_utils import DisplayUtils
+from views.layouts.display_layout import DisplayLayout
+from views.layouts.button_layout import ButtonLayout
+
+class MainLayout(QVBoxLayout):
+  def __init__(self):
+    super().__init__()
+
+    self.setSpacing(0)
+
+    displayLayout = DisplayLayout()
+    displayUtils = DisplayUtils(displayLayout)
+    buttonLayout = ButtonLayout(displayUtils)
+
+    displayWidget = QWidget()
+    buttonWidget = QWidget()
+    
+    displayWidget.setLayout(displayLayout)
+    buttonWidget.setLayout(buttonLayout)
+
+    self.addWidget(displayWidget, 30)
+    self.addWidget(buttonWidget, 70)
--- app/views/window.py
+++ app/views/window.py
@@ -1,19 +1,24 @@
-from views.calculator.widget import Widget as CalculatorWidget
 from PySide6.QtGui import QIcon
-from qframelesswindow import FramelessMainWindow, StandardTitleBar
+from utils.display_utils import DisplayUtils
+from views.calculator.widget import Layout
+from views.calculator.widget import Widget as CalculatorWidget
+from views.layouts.main_layout import MainLayout
+from qframelesswindow.windows import WindowsFramelessWindow
+from qframelesswindow.titlebar import StandardTitleBar
 
-class Window(FramelessMainWindow):
+class Window(WindowsFramelessWindow):
   def __init__(self):
     super().__init__()
     
-    self.setTitleBar(StandardTitleBar(self))
-    
-    self.setWindowTitle("Calculator")
-    self.setWindowIcon(QIcon(":/images/logo"))
-
-    self.setCentralWidget(CalculatorWidget())
-    self.centralWidget().setContentsMargins(0,32,0,0)
+    self.setLayout(MainLayout())
+    self.layout().setContentsMargins(0,32,0,0)
 
     self.setFixedSize(250, 350)
 
+    self._init_titlebar()
+
+  def _init_titlebar(self):
+    self.setTitleBar(StandardTitleBar(self))
+    self.setWindowIcon(QIcon(":/images/logo"))
+    self.setWindowTitle("Calculator")
     self.titleBar.raise_()
